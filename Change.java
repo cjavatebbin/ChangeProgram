1367b00cb916ede7d43a46873742086b8958f14e
jpolacco <jpolacco@gmail.com>
2020-08-17 22:50:19 -0700

    Add files via upload

---
+++ Change.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Change here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Change{
+    
+    
+   /**
+    * converts cents to quarters, dimes, nickels
+    * 
+    * precondition: cents is between 0 and 75, inclusive
+    * 
+    * @param cents the number of cents
+    * @return a String in the form "q quarters, d dimes, and n nickels"
+    */ 
+    public String cashInCoins(int cents){
+        // TODO
+        return ""; // delete this line of code
+    }
+        
+}
---
+++ ChangeMain.java
@@ -0,0 +1,25 @@
+/**
+ * Write a program that determines the change to be dispensed from a vending 
+ * machine. An item in the machine can cost between 25 cents and 1 dollar, in 5-cent
+ * increments (25, 30, 35, . . . , 90, 95, or 100), and the machine accepts only a 
+ * single dollar bill to pay for the item. For example, a possible sample dialogue
+ * might be the following:
+ * Enter price of item (from 25 cents to a dollar, in 5-cent increments): 45
+ * You bought an item for 45 cents and gave me a dollar, so your change is 
+ * 2 quarters, 0 dimes, and 1 nickels.
+ * 
+ * @author (your name here)
+ * @version (date here)
+ */
+
+import java.util.Scanner;
+
+public class ChangeMain
+{
+   public static void main(String[] args){
+       //TODO
+       // Note that the cashInCoins method of the Change class doesn't determine the change
+       // returned. Its job is simply to convert cents to coins. 
+    }
+       
+}
